bcde16f1725fd681975d89677f148585619e97cd
Katie Vargas <you@example.com>
2023-11-16 21:24:19 -0500

    updated changes

--- freelancerforum.js
+++ freelancerforum.js
@@ -1,10 +1,43 @@
-const freelancers = [
-    { name: "Dr. Slice", price: 25, occupation: "gardener" },
-    { name: "Dr. Pressure", price: 51, occupation: "programmer" },
-    { name: "Prof. Possibility", price: 43, occupation: "teacher" },
-    { name: "Prof. Prism", price: 81, occupation: "teacher" },
-    { name: "Dr. Impulse", price: 43, occupation: "teacher" },
-    { name: "Prof. Spark", price: 76, occupation: "programmer" },
-    { name: "Dr. Wire", price: 47, occupation: "teacher" },
-    { name: "Prof. Goose", price: 72, occupation: "driver" },
-  ];
+const state = {
+  freelancers: [
+    {
+      name: 'Alice',
+      occupation: 'Writer',
+      price: 30,
+    },
+    {
+      name: 'Bob',
+      occupation: 'Teacher',
+      price: 50,
+    },
+    {
+      name: 'Carol',
+      occupation: 'Programmer',
+      price: 70,
+    }
+  ],
+}
+
+function updateAveragePrice(){
+    let total = 0;
+
+    for (let 1 = 0; i < state.freelancers.length; ++i) {
+      const currentFreelancer = state.freelancers[i];
+
+      total +=
+    }
+}
+
+function updateAverageText() {
+
+}
+
+function addFreelancerRow(freelancer) {
+    state.freelancers.push(freelancer)
+
+}
+
+function render() {
+    const table = document.getElementById( elementId 'freelancer_table');
+}
+
